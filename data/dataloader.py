656b6495e68621faa6420356c46a6723002ac154
cdsnlab <cdsnlab@gmail.com>
2022-01-21 23:46:03 +0900

    commit dataloader

---
+++ data/dataloader.py
@@ -0,0 +1,20 @@
+def laprasLoader():
+    
+    print("Loading Lapras Dataset")
+
+    return 'train', 'test'
+
+def casasLoader():
+    print("Loading Casas Dataset")
+
+    return 'train', 'test'
+
+def arasLoader():
+    print("Loading Aras Dataset")
+
+    return 'train', 'test'
+
+def opportunityLoader():
+    print("Loading Opportunity Dataset")
+
+    return 'train', 'test'
