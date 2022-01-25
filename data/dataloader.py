a03bbf01e5711105b006fb7591a78a24e1361c0b
cdsnlab <cdsnlab@gmail.com>
2022-01-25 09:39:23 +0900

    update variable and class

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,3 +1,23 @@
+from fileinput import filename
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+import numpy as np
+import csv
+from glob import glob
+
+# Static variable
+timespan = 1000 # for each timespan sec (1000==1 sec)
+len_th = 10 # minimum sequence length
+
+# for storing dataset element
+class TSDataSet:
+    def __init__(self,data, label, length):
+        self.data = data
+        self.label = label
+        self.length= length
+        
 # Lapras data format : Sensor type, context name, start time, end time / file name = activity label
 # Examples(csv) : Seat Occupy,1,1.490317862115E12,1.490319250294E12,23.136316666666666
 def laprasLoader():
