b9b542b7199d28817a96d28e6adb7cd8a0133b67
iplay93 <iplay93@gmail.com>
2022-01-26 01:14:56 +0900

    update count_label function and minor revision

--- data/dataloader.py
+++ data/dataloader.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 # Static variable
 timespan = 1000 # for each timespan sec (1000==1 sec)
 len_th = 10 # minimum sequence length
+min_samples = 10 # minimum # of samples
 
 # for storing dataset element
 class TSDataSet:
@@ -29,6 +30,7 @@ def label_num(filename):
 
 # Lapras data format : Sensor type, context name, start time, end time / file name = activity label : [1 : 'Chatting', 2: 'Discussion', 3: 'GroupStudy', 4: 'Presentation', 5: 'NULL']
 # Examples(csv) : Seat Occupy,1,1.490317862115E12,1.490319250294E12,23.136316666666666
+#[1, 2, 3, 5, 4] : activity #, [119, 52, 40, 116, 129], # of activities
 def laprasLoader(file_name):    
     print("Loading Lapras Dataset--------------------------------------")
     # variable initialization
@@ -78,7 +80,10 @@ def laprasLoader(file_name):
                     state_list.append(temp_df[i, 1])
 
         time_list.append([start_time, end_time])
-
+    # print(item_list)
+    # print(state_list)
+    # print(label_list)
+    # print(time_list)
     item_list= ['Seat Occupy', 'Sound', 'Brightness', 'Light', 'Existence', 'Projector', 'Presentation']
 
     count_file = 0
@@ -116,8 +121,10 @@ def laprasLoader(file_name):
     print("Loading Lapras Dataset Finished--------------------------------------")
     return dataset_list
 
+
 # CASAS data format : timestamp(when activated), sensor type+context name, state, user #, activity label : [1-15] / file name = day 
 # Examples(txt) : 2008-11-10 14:28:17.986759 M22 ON 2 2 
+# [1, 4, 6, 9, 10, 12, 14, 13, 2, 3, 7, 8, 11, 15, 5] : activity #, [50, 26, 27, 24, 26, 34, 26, 43, 26, 26, 26, 24, 27, 26, 17] : # of activities
 def casasLoader(file_name):
     print("Loading Casas Dataset--------------------------------------")
     # variable initialization
@@ -176,7 +183,7 @@ def casasLoader(file_name):
                     
                     # if the row is related to the resident
                     if(len(temp_list)==3 and int(temp_list[1])-1 == rid) or (len(temp_list)>3 and (int(temp_list[1])-1 == rid or int(temp_list[3])-1 == rid)):
-                        print(rid,":", file, i, temp_list)
+                        #print(rid,":", file, i, temp_list)
                         if(current_label==0):
                             
                             # for the first row
@@ -193,8 +200,7 @@ def casasLoader(file_name):
                             temp_dataset = np.array([activity_list]) # sensor sequence                   
                         
                         # if the same activity continue                
-                        if((current_label == int(temp_list[2])) or (len(temp_list)>3 and current_label == int(temp_list[4]))):
-                            
+                        if((current_label == int(temp_list[2])) or (len(temp_list)>3 and current_label == int(temp_list[4]))):                            
                             if(temp_list[0] in ['ON', 'OPEN', 'PRESENT']): # when ['ON', 'OPEN', 'PRESENT']
                                 activity_list[item_list.index(temp_df[i, 2])] = 1    
                             else:   # when ['OFF', 'ABSENT', 'CLOSE']
@@ -205,7 +211,7 @@ def casasLoader(file_name):
                             if(len(temp_dataset)>len_th):
                                 # construct new object(for old activity)          
                                 dataset_list.append(TSDataSet(temp_dataset, current_label, len(temp_dataset)))
-                                print(rid, current_label, len(temp_dataset))
+                                #print(rid, current_label, len(temp_dataset))
                                 # just for show 
                                 label_list.append(current_label)          
                                             
@@ -225,12 +231,17 @@ def casasLoader(file_name):
                     dataset_list.append(TSDataSet(temp_dataset, current_label, len(temp_dataset)))
                     # just for show
                     label_list.append(current_label)
-                    print(rid, current_label, len(temp_dataset))
+                    #print(rid, current_label, len(temp_dataset))
     print("Loading CASAS Dataset Finished--------------------------------------")
     return dataset_list
 
+
 # ARAS data format : (for each second) sensor type+context name,..., activity label1, activity label2 : [1-27]/ file name = day 
 # Examples(txt) : 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 13 17
+# A: [1217, 2217, 1215, 2117, 1517, 1717, 1711, 1111, 1120, 1121, 1115, 1127, 1102, 102, 1202, 302, 402, 1502, 1702, 2102, 1302, 2202, 1902, 2702, 202, 214, 217, 212, 222, 210, 112, 712, 717, 715, 708, 808, 809, 817, 2221, 2212, 1212, 2112, 2622, 2722, 122, 1522, 1527, 2627, 2612, 1012, 2615, 2620, 2601, 2617, 2610, 1210, 1110, 1101, 902, 1002, 2402, 1402, 1422, 1427, 1418, 2525, 1825, 1818, 1812, 2717, 2727, 1722, 1222, 2215, 1214, 1221, 2511, 1511, 2111, 1104, 502, 227, 201, 2712, 1512, 1712, 1707, 707, 818, 1218, 1209, 922, 2222, 1710, 1718, 1211, 602, 1327, 1322, 2522, 1407, 702, 908, 909, 912, 215, 1510, 1509, 1521, 727, 722, 822, 812, 918, 1022, 1017, 1802, 2527, 1727, 1716, 116, 2216, 2214, 114, 1714, 109, 1721, 2121, 121, 115, 802, 1816, 1216, 2218, 2211, 1116, 117, 1715, 1701, 2302, 1516, 1817, 1814, 1810, 2116, 2110, 1112, 1815, 312, 412, 416, 916, 1412, 512, 612, 1827, 101, 1801, 1822, 1709, 1602, 110, 2201, 1109, 1227, 1027, 1015, 1201, 1207, 921, 2210, 1821, 127, 1501, 1010, 1312, 120, 2209, 1807, 107, 1001, 2101, 1614, 1610, 316, 317, 401, 415, 901, 1401, 2701, 1118, 1122, 301, 2118, 207, 1507, 2707, 2207, 1009, 1114, 1011, 1627, 1601, 2227, 1310, 1309, 1301, 1321, 1311, 911, 2311, 718, 716, 216, 915, 2721, 1411, 111, 927, 1315, 209, 221, 2602, 2517, 1103, 2715, 1317, 1325, 2515, 1314, 1125, 2503, 1503, 404, 421, 920, 1720, 1713, 1213, 113, 2213, 1723, 1316, 1708, 2308, 2309, 1518, 322, 422, 2127, 2719, 2312, 1719, 1813, 1220, 2307, 1108, 2512, 522, 517, 617, 917, 1318]
+# A: [18, 16, 16, 4, 6, 19, 18, 35, 18, 13, 43, 22, 28, 61, 90, 34, 24, 71, 94, 32, 40, 69, 5, 37, 43, 2, 5, 15, 9, 6, 15, 7, 3, 2, 1, 12, 5, 1, 4, 34, 59, 5, 2, 3, 4, 2, 2, 1, 2, 13, 1, 1, 2, 2, 1, 11, 9, 18, 41, 25, 3, 18, 4, 3, 1, 16, 1, 3, 15, 1, 5, 12, 23, 6, 3, 7, 1, 6, 14, 3, 23, 7, 12, 8, 19, 49, 7, 10, 2, 4, 5, 3, 12, 10, 5, 4, 17, 4, 6, 2, 1, 9, 3, 7, 11, 4, 4, 3, 3, 2, 3, 1, 3, 2, 2, 5, 12, 3, 8, 6, 6, 5, 5, 2, 7, 2, 6, 2, 1, 5, 5, 4, 13, 3, 5, 11, 3, 10, 10, 6, 8, 3, 4, 3, 4, 1, 3, 2, 4, 3, 1, 1, 6, 3, 2, 3, 9, 6, 5, 5, 5, 3, 6, 4, 7, 3, 1, 6, 3, 2, 6, 1, 4, 4, 6, 8, 2, 2, 1, 4, 1, 3, 1, 1, 1, 1, 1, 1, 2, 2, 4, 2, 1, 1, 1, 1, 1, 1, 1, 3, 2, 3, 2, 1, 2, 2, 1, 7, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 3, 1, 1, 2, 1, 2, 1, 6, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 4, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# B: [1111, 1511, 111, 1115, 1118, 118, 1518, 318, 418, 404, 918, 115, 1318, 1315, 1313, 513, 606, 106, 101, 901, 913, 1316, 1516, 1310, 1018, 1218, 1215, 1212, 1512, 117, 1517, 317, 2525, 2727, 2127, 2121, 121, 202, 1527, 2715, 2701, 701, 801, 808, 1201, 1101, 1117, 127, 2118, 315, 1715, 1804, 1817, 1317, 1321, 1417, 2717, 2101, 1217, 2714, 114, 1814, 1827, 1801, 1501, 2501, 1112, 2711, 2702, 1102, 1127, 2111, 102, 1502, 1103, 1104, 1120, 302, 402, 902, 1802, 1302, 1002, 502, 602, 702, 802, 1202, 2102, 311, 401, 1525, 2515, 518, 515, 112, 1012, 1504, 1211, 1311, 1011, 1818, 1121, 1401, 1702, 1902, 1402, 1015, 1027, 1001, 2112, 411, 1811, 1322, 1304, 517, 915, 717, 815, 1427, 1727, 1701, 2712, 1712, 1227, 304, 1301, 1327, 2302, 917, 1717, 1815, 2415, 2418, 1918, 1418, 2718, 715, 912, 301, 1721, 727, 712, 108, 1708, 1017, 1116, 2716, 316, 104, 120, 920, 718, 1208, 1416, 116, 2211, 921, 2721, 1412, 1513, 413]
+# B: [43, 21, 9, 26, 3, 13, 11, 2, 1, 17, 2, 20, 13, 8, 2, 1, 3, 1, 55, 6, 1, 2, 3, 1, 4, 3, 9, 39, 10, 8, 16, 8, 14, 15, 7, 1, 2, 29, 8, 8, 9, 1, 2, 4, 2, 11, 10, 23, 2, 6, 1, 4, 7, 14, 1, 2, 7, 7, 2, 4, 5, 1, 1, 3, 14, 1, 5, 2, 12, 9, 4, 2, 33, 31, 1, 1, 2, 4, 4, 2, 3, 24, 7, 3, 3, 6, 5, 25, 5, 7, 3, 1, 1, 1, 1, 8, 1, 2, 1, 7, 1, 3, 1, 2, 1, 2, 2, 1, 1, 1, 3, 1, 2, 1, 1, 1, 1, 4, 2, 1, 3, 1, 4, 2, 3, 2, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 def arasLoader(file_name):   
     print("Loading ARAS Dataset--------------------------------------")
     # variable initialization
@@ -295,6 +306,8 @@ def arasLoader(file_name):
 # Opportunity data format : sensor type+context name,..., activity label(ADL)/ file name = each user(4) * 5
 # Examples(txt) : 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 13 17
 # the number of examples : 101(Relaxing) - 40, 102(Coffee time)-20, 103(Early morning)-20, 104(Cleanup)-20, 105(Sandwich time)-20
+# [1, 3, 2, 5, 4] : activity #
+# [40, 20, 20, 20, 20] : # of activity 
 def opportunityLoader(file_name):
     print("Loading Opportunity Dataset --------------------------------------")
     # variable initialization
@@ -313,6 +326,7 @@ def opportunityLoader(file_name):
         file_list.append(x)
     # sorting by file name
     file_list.sort()
+
 
     # for each file
     for file in file_list :
@@ -354,7 +368,24 @@ def opportunityLoader(file_name):
     print("Loading Opportunity Dataset Finished--------------------------------------")
     return dataset_list
 
-# visualization of each activity
+class TimeseriesDataset(Dataset):   
+    def __init__(self, data, window, target_cols):
+        self.data = torch.Tensor(data)
+        self.window = window
+        self.target_cols = target_cols
+        self.shape = self.__getshape__()
+        self.size = self.__getsize__() 
+    def __getitem__(self, index):
+        x = self.data[index:index+self.window]
+        y = self.data[index+self.window,0:target_cols]
+        return x, y 
+    def __len__(self):
+        return len(self.data) -  self.window     
+    def __getshape__(self):
+        return (self.__len__(), *self.__getitem__(0)[0].shape)    
+    def __getsize__(self):
+        return (self.__len__())
+
 def visualization_data(dataset_list, file_name, activity_num):
     print("Visualizing Dataset --------------------------------------")
     label_count = [0 for x in range(activity_num)]
@@ -381,28 +412,74 @@ def visualization_data(dataset_list, file_name, activity_num):
 
     print("Visualizing Dataset Finished--------------------------------------")
 
+def count_label(dataset_list):
+    # finding types and counts of label
+    types_label_list =[]
+    count_label_list = []
+    for i in range(len(dataset_list)):
+        if(dataset_list[i].label not in types_label_list):
+            types_label_list.append(dataset_list[i].label)
+            count_label_list.append(1)
+        else:
+            count_label_list[types_label_list.index(dataset_list[i].label)]+=1
+
+    print('types_label :', types_label_list)
+    print('count_label :', count_label_list)   
+                
+    return types_label_list, count_label_list
+
+def count_label_labellist(datalist, labellist):
+    # finding types and counts of label
+    types_label_list =[]
+    count_label_list = []
+    for i in range(len(datalist)):
+        if(labellist[i] not in types_label_list):
+            types_label_list.append(labellist[i])
+            count_label_list.append(1)
+        else:
+            count_label_list[types_label_list.index(labellist[i])]+=1
+
+    print('types_label :', types_label_list)
+    print('count_label :', count_label_list)   
+                
+    return types_label_list, count_label_list
+
 # split data into train/validate/test 
 def splitting_data(dataset, test_ratio, valid_ratio, seed):    
     if dataset == 'lapras':
         dataset_list = laprasLoader('KDD2022/data/Lapras/*.csv')
-        visualization_data(dataset_list, 'KDD2022/data/Lapras/', 5)
+        #visualization_data(dataset_list, 'KDD2022/data/Lapras/', 5)
     elif dataset == 'casas':
         dataset_list = casasLoader('KDD2022/data/CASAS/*.txt')
-        visualization_data(dataset_list, 'KDD2022/data/CASAS/', 15)
+        #visualization_data(dataset_list, 'KDD2022/data/CASAS/', 15)
     elif dataset == 'aras_a':
         dataset_list = arasLoader('KDD2022/data/Aras/HouseA/*.txt')
-        visualization_data(dataset_list, 'KDD2022/data/Aras/HouseA/', 27*100 + 27)
+        #visualization_data(dataset_list, 'KDD2022/data/Aras/HouseA/', 27*100 + 27)
     elif dataset == 'aras_b':
         dataset_list = arasLoader('KDD2022/data/Aras/HouseB/*.txt')
-        visualization_data(dataset_list, 'KDD2022/data/Aras/HouseB/', 27*100 + 27)
+        #visualization_data(dataset_list, 'KDD2022/data/Aras/HouseB/', 27*100 + 27)
     elif dataset == 'opportunity':
         dataset_list = opportunityLoader('KDD2022/data/Opportunity/*.dat')
-        visualization_data(dataset_list, 'KDD2022/data/Opportunity/', 5)
+        #visualization_data(dataset_list, 'KDD2022/data/Opportunity/', 5)
+    
+    types_label_list, count_label_list = count_label(dataset_list)
+    
+    # Convert object-list to list-list
+    datalist=[]
+    labellist=[]
+    for i in range(len(dataset_list)):
+        # Select datalist by min_samples
+        if(count_label_list[types_label_list.index(dataset_list[i].label)]> min_samples):
+            datalist.append(dataset_list[i].data)
+            labellist.append(dataset_list[i].label)
+    
+    count_label_labellist(datalist, labellist)
+    # Split train and valid dataset
+    train_list, test_list, train_label_list, test_label_list = train_test_split(datalist, labellist, test_size=test_ratio, stratify= labellist, random_state=seed)
+    train_list, valid_list, train_label_list, valid_label_list= train_test_split(train_list, train_label_list, test_size=valid_ratio, stratify=train_label_list, random_state=seed)
 
-    # Split train and valid dataset 
-    train_list, test_list = train_test_split(dataset_list, labels,test_size=test_ratio, stratify=labels, random_state=seed)
-    train_list, valid_list = train_test_split(train_list, labels, test_size=valid_ratio, stratify=labels, random_state=seed)
-    print(f"Train Data: {len(train_list)}") # 0.8*0.8 = 0.64
-    print(f"Validation Data: {len(valid_list)}") # 0.8*0.2 = 0.16
-    print(f"Test Data: {len(test_list)}") # 0.2 
+    print(f"Train Data: {len(train_list)}") 
+    print(f"Validation Data: {len(valid_list)}") 
+    print(f"Test Data: {len(test_list)}") 
+    
     return train_list, valid_list, test_list
