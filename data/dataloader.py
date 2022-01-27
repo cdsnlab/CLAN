3ff51ec59827573c1497d0ac639bb74eb43c9f18
iplay93 <iplay93@gmail.com>
2022-01-27 17:57:24 +0900

    add padding function

--- data/dataloader.py
+++ data/dataloader.py
@@ -6,11 +6,12 @@ import numpy as np
 import csv
 from glob import glob
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 # Static variable
-timespan = 1000 # for each timespan sec (1000==1 sec)
-len_th = 10 # minimum sequence length
-min_samples = 10 # minimum # of samples
+#timespan = 1000 # for each timespan sec (1000==1 sec)
+#min_seq = 10 # minimum sequence length
+#min_samples = 10 # minimum # of samples
 
 # for storing dataset element
 class TSDataSet:
@@ -42,7 +43,8 @@ def min_max_scaling(df):
 # Lapras data format : Sensor type, context name, start time, end time / file name = activity label : [1 : 'Chatting', 2: 'Discussion', 3: 'GroupStudy', 4: 'Presentation', 5: 'NULL']
 # Examples(csv) : Seat Occupy,1,1.490317862115E12,1.490319250294E12,23.136316666666666
 #[1, 2, 3, 5, 4] : activity #, [119, 52, 40, 116, 129], # of activities
-def laprasLoader(file_name):    
+def laprasLoader(file_name, timespan, min_seq):
+    
     print("Loading Lapras Dataset--------------------------------------")
     # variable initialization
     file_list = [] # store file names
@@ -123,7 +125,7 @@ def laprasLoader(file_name):
                     elif(temp_df[i,0] == 'Light' or temp_df[i,0] == 'Projector' or temp_df[i,0] == 'Presentation'):
                         temp_dataset[j][item_list.index(temp_df[i, 0])] = 1
 
-            if(len(temp_dataset)> len_th):
+            if(len(temp_dataset)> min_seq):
                 current_label = label_list[count_file] # label list  
                 dataset_list.append(TSDataSet(temp_dataset, current_label, len(temp_dataset)))
         # for next file
@@ -136,7 +138,7 @@ def laprasLoader(file_name):
 # CASAS data format : timestamp(when activated), sensor type+context name, state, user #, activity label : [1-15] / file name = day 
 # Examples(txt) : 2008-11-10 14:28:17.986759 M22 ON 2 2 
 # [1, 4, 6, 9, 10, 12, 14, 13, 2, 3, 7, 8, 11, 15, 5] : activity #, [50, 26, 27, 24, 26, 34, 26, 43, 26, 26, 26, 24, 27, 26, 17] : # of activities
-def casasLoader(file_name):
+def casasLoader(file_name,timespan, min_seq):
     print("Loading CASAS Dataset--------------------------------------")
     # variable initialization
     file_list = [] # store file names
@@ -219,7 +221,7 @@ def casasLoader(file_name):
                             temp_dataset = np.concatenate((temp_dataset, [activity_list]), axis=0)
                         # if the activity is finished (new activity arrival)
                         else:                        
-                            if(len(temp_dataset)>len_th):
+                            if(len(temp_dataset)>min_seq):
                                 # construct new object(for old activity)          
                                 dataset_list.append(TSDataSet(temp_dataset, current_label, len(temp_dataset)))
                                 #print(rid, current_label, len(temp_dataset))
@@ -237,7 +239,7 @@ def casasLoader(file_name):
                                 current_label =  int(temp_list[2]) # 2 column is the label
                             elif(int(temp_list[3])-1 == rid):
                                 current_label =  int(temp_list[4])    
-                if(len(temp_dataset)>len_th):
+                if(len(temp_dataset)>min_seq):
                     # for the last activity
                     dataset_list.append(TSDataSet(temp_dataset, current_label, len(temp_dataset)))
                     # just for show
@@ -253,7 +255,7 @@ def casasLoader(file_name):
 # A: [18, 16, 16, 4, 6, 19, 18, 35, 18, 13, 43, 22, 28, 61, 90, 34, 24, 71, 94, 32, 40, 69, 5, 37, 43, 2, 5, 15, 9, 6, 15, 7, 3, 2, 1, 12, 5, 1, 4, 34, 59, 5, 2, 3, 4, 2, 2, 1, 2, 13, 1, 1, 2, 2, 1, 11, 9, 18, 41, 25, 3, 18, 4, 3, 1, 16, 1, 3, 15, 1, 5, 12, 23, 6, 3, 7, 1, 6, 14, 3, 23, 7, 12, 8, 19, 49, 7, 10, 2, 4, 5, 3, 12, 10, 5, 4, 17, 4, 6, 2, 1, 9, 3, 7, 11, 4, 4, 3, 3, 2, 3, 1, 3, 2, 2, 5, 12, 3, 8, 6, 6, 5, 5, 2, 7, 2, 6, 2, 1, 5, 5, 4, 13, 3, 5, 11, 3, 10, 10, 6, 8, 3, 4, 3, 4, 1, 3, 2, 4, 3, 1, 1, 6, 3, 2, 3, 9, 6, 5, 5, 5, 3, 6, 4, 7, 3, 1, 6, 3, 2, 6, 1, 4, 4, 6, 8, 2, 2, 1, 4, 1, 3, 1, 1, 1, 1, 1, 1, 2, 2, 4, 2, 1, 1, 1, 1, 1, 1, 1, 3, 2, 3, 2, 1, 2, 2, 1, 7, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 3, 1, 1, 2, 1, 2, 1, 6, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 4, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 # B: [1111, 1511, 111, 1115, 1118, 118, 1518, 318, 418, 404, 918, 115, 1318, 1315, 1313, 513, 606, 106, 101, 901, 913, 1316, 1516, 1310, 1018, 1218, 1215, 1212, 1512, 117, 1517, 317, 2525, 2727, 2127, 2121, 121, 202, 1527, 2715, 2701, 701, 801, 808, 1201, 1101, 1117, 127, 2118, 315, 1715, 1804, 1817, 1317, 1321, 1417, 2717, 2101, 1217, 2714, 114, 1814, 1827, 1801, 1501, 2501, 1112, 2711, 2702, 1102, 1127, 2111, 102, 1502, 1103, 1104, 1120, 302, 402, 902, 1802, 1302, 1002, 502, 602, 702, 802, 1202, 2102, 311, 401, 1525, 2515, 518, 515, 112, 1012, 1504, 1211, 1311, 1011, 1818, 1121, 1401, 1702, 1902, 1402, 1015, 1027, 1001, 2112, 411, 1811, 1322, 1304, 517, 915, 717, 815, 1427, 1727, 1701, 2712, 1712, 1227, 304, 1301, 1327, 2302, 917, 1717, 1815, 2415, 2418, 1918, 1418, 2718, 715, 912, 301, 1721, 727, 712, 108, 1708, 1017, 1116, 2716, 316, 104, 120, 920, 718, 1208, 1416, 116, 2211, 921, 2721, 1412, 1513, 413]
 # B: [43, 21, 9, 26, 3, 13, 11, 2, 1, 17, 2, 20, 13, 8, 2, 1, 3, 1, 55, 6, 1, 2, 3, 1, 4, 3, 9, 39, 10, 8, 16, 8, 14, 15, 7, 1, 2, 29, 8, 8, 9, 1, 2, 4, 2, 11, 10, 23, 2, 6, 1, 4, 7, 14, 1, 2, 7, 7, 2, 4, 5, 1, 1, 3, 14, 1, 5, 2, 12, 9, 4, 2, 33, 31, 1, 1, 2, 4, 4, 2, 3, 24, 7, 3, 3, 6, 5, 25, 5, 7, 3, 1, 1, 1, 1, 8, 1, 2, 1, 7, 1, 3, 1, 2, 1, 2, 2, 1, 1, 1, 3, 1, 2, 1, 1, 1, 1, 4, 2, 1, 3, 1, 4, 2, 3, 2, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-def arasLoader(file_name):   
+def arasLoader(file_name, timespan, min_seq):   
     print("Loading ARAS Dataset--------------------------------------")
     # variable initialization
     file_list = [] # store file names
@@ -294,7 +296,7 @@ def arasLoader(file_name):
                         temp_dataset = np.concatenate((temp_dataset, [np.array(temp_df[i,0:19])]), axis=0)    
                     # if the activity is finished (new activity arrival)                   
                     else:
-                        if(len(temp_dataset)>len_th):
+                        if(len(temp_dataset)>min_seq):
                             # construct new object(for old activity)          
                             dataset_list.append(TSDataSet(temp_dataset, (current_label[0]*100+ current_label[1]), len(temp_dataset)))
                             # just for show 
@@ -305,7 +307,7 @@ def arasLoader(file_name):
                         current_label[0] = temp_df[i, 20] 
                         current_label[1] = temp_df[i, 21]
 
-            if(len(temp_dataset)>len_th):
+            if(len(temp_dataset)>min_seq):
                 # for the last activity
                 dataset_list.append(TSDataSet(temp_dataset, (current_label[0]*100+ current_label[1]), len(temp_dataset)))
                 # just for show
@@ -319,7 +321,7 @@ def arasLoader(file_name):
 # the number of examples : 101(Relaxing) - 40, 102(Coffee time)-20, 103(Early morning)-20, 104(Cleanup)-20, 105(Sandwich time)-20
 # [1, 3, 2, 5, 4] : activity #
 # [40, 20, 20, 20, 20] : # of activity 
-def opportunityLoader(file_name):
+def opportunityLoader(file_name, timespan, min_seq):
     print("Loading Opportunity Dataset --------------------------------------")
     # variable initialization
     file_list = [] # store file names
@@ -455,22 +457,86 @@ def count_label_labellist(datalist, labellist):
                 
     return types_label_list, count_label_list
 
+def padding_by_max(lengthlist, normalized_df):
+    # reconstruction of datalist    
+    datalist=[]
+    reconst_list =[]
+    count_lengthlist = 0
+    print("max", max(lengthlist))
+    # reconstruction of normalized list
+    # for each row
+    for i in range(len(lengthlist)):
+        reconst_list =[]    
+        # cut df by each length
+        for j in range(count_lengthlist,(count_lengthlist+lengthlist[i])):
+            reconst_list.append(normalized_df.iloc[j,:].tolist())            
+        count_lengthlist += lengthlist[i]
+
+        #padding to each data list
+        if((max(lengthlist)-lengthlist[i])%2 == 0):
+            p2d = (0, 0, int((max(lengthlist)-lengthlist[i])/2), int((max(lengthlist)-lengthlist[i])/2))
+        else :
+            p2d = (0, 0, int((max(lengthlist)-lengthlist[i]+1)/2)-1, int((max(lengthlist)-lengthlist[i]+1)/2))
+        datalist.append(F.pad(torch.tensor(reconst_list),p2d,"constant",-1))
+    
+    return datalist
+
+def padding_by_mean(lengthlist, normalized_df):
+    # reconstruction of datalist    
+    datalist=[]
+    reconst_list =[]
+    count_lengthlist = 0
+    mean_length = int(sum(lengthlist)/len(lengthlist))
+    print("mean", mean_length)
+    for i in range(len(lengthlist)):
+        reconst_list =[]    
+        # cut df by each length
+        if(lengthlist[i]>=mean_length): # length is larger than mean
+            for j in range(count_lengthlist, count_lengthlist+mean_length):
+                reconst_list.append(normalized_df.iloc[j,:].tolist())
+            datalist.append(torch.tensor(reconst_list))
+        else: # length is smaller than mean
+            for j in range(count_lengthlist, (count_lengthlist+lengthlist[i])):
+                reconst_list.append(normalized_df.iloc[j,:].tolist())
+            # padding to the end 
+            p2d = (0, 0, 0, mean_length-lengthlist[i])
+            datalist.append(F.pad(torch.tensor(reconst_list),p2d,"constant",-1))    
+        count_lengthlist += lengthlist[i]    
+    return datalist
+
+def reconstrct_list(lengthlist, normalized_df):
+    # reconstruction of datalist    
+    datalist=[]
+    reconst_list =[]
+    count_lengthlist = 0
+    # for each row
+    for i in range(len(lengthlist)):
+        reconst_list =[]    
+        # cut df by each length
+        for j in range(count_lengthlist,(count_lengthlist+lengthlist[i])):
+            reconst_list.append(normalized_df.iloc[j,:].tolist())            
+        count_lengthlist += lengthlist[i]
+        datalist.append(torch.tensor(reconst_list))
+    return datalist
+
 # split data into train/validate/test 
-def splitting_data(dataset, test_ratio, valid_ratio, seed):    
+def splitting_data(dataset, test_ratio, valid_ratio, padding, seed, timespan, min_seq, min_samples): 
+
+    print(timespan, min_seq, min_samples)
     if dataset == 'lapras':
-        dataset_list = laprasLoader('data/Lapras/*.csv')
+        dataset_list = laprasLoader('data/Lapras/*.csv', timespan, min_seq)
         #visualization_data(dataset_list, 'KDD2022/data/Lapras/', 5)
     elif dataset == 'casas':
-        dataset_list = casasLoader('data/CASAS/*.txt')
+        dataset_list = casasLoader('data/CASAS/*.txt', timespan, min_seq)
         #visualization_data(dataset_list, 'KDD2022/data/CASAS/', 15)
     elif dataset == 'aras_a':
-        dataset_list = arasLoader('data/Aras/HouseA/*.txt')
+        dataset_list = arasLoader('data/Aras/HouseA/*.txt', timespan, min_seq)
         #visualization_data(dataset_list, 'KDD2022/data/Aras/HouseA/', 27*100 + 27)
     elif dataset == 'aras_b':
-        dataset_list = arasLoader('data/Aras/HouseB/*.txt')
+        dataset_list = arasLoader('data/Aras/HouseB/*.txt', timespan, min_seq)
         #visualization_data(dataset_list, 'KDD2022/data/Aras/HouseB/', 27*100 + 27)
     elif dataset == 'opportunity':
-        dataset_list = opportunityLoader('data/Opportunity/*.dat')
+        dataset_list = opportunityLoader('data/Opportunity/*.dat', timespan, min_seq)
         #visualization_data(dataset_list, 'KDD2022/data/Opportunity/', 5)
     
     types_label_list, count_label_list = count_label(dataset_list)
@@ -487,26 +553,19 @@ def splitting_data(dataset, test_ratio, valid_ratio, seed):
             labellist.append(dataset_list[i].label)
             lengthlist.append(len(dataset_list[i].data))
             for j in range(len(dataset_list[i].data)):
-                templist.append(dataset_list[i].data[j])
-     
+                templist.append(dataset_list[i].data[j])     
                
     # normalization of dataframe
     normalized_df = min_max_scaling(pd.DataFrame(templist))
     normalized_df = normalized_df.fillna(0)
 
-    # reconstruction of datalist    
-    datalist=[]
-    reconst_list =[]
-    count_lengthlist = 0
-    # for each row
-    for i in range(len(lengthlist)):
-        reconst_list =[]    
-        # cut df by each length
-        for j in range(count_lengthlist,(count_lengthlist+lengthlist[i])):
-            reconst_list.append(normalized_df.iloc[j,:].tolist())    
-        
-        count_lengthlist += lengthlist[i]
-        datalist.append(reconst_list)
+    # reconstruction of list (padding is option : max or mean)
+    if padding == 'max':
+        datalist = padding_by_max(lengthlist, normalized_df)
+    elif padding =='mean':
+        datalist = padding_by_mean(lengthlist, normalized_df)
+    else:
+        datalist = reconstrct_list(lengthlist, normalized_df)
 
     count_label_labellist(datalist, labellist)
     # Split train and valid dataset
