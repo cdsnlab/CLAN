0146654fe8f55ab8adff0ee60d77777266ec7d39
iplay93 <iplay93@gmail.com>
2022-03-29 14:33:50 +0900

    early_stopping_visualization update

--- utils/visualization.py
+++ utils/visualization.py
@@ -7,3 +7,22 @@ from sklearn.manifold import TSNE
 import seaborn as sns
 from matplotlib import cm
 import torch
+
+def early_stopping_visualization(train_loss, valid_loss):
+    fig = plt.figure(figsize=(10,8))
+    plt.plot(range(1,len(train_loss)+1),train_loss, label='Training Loss')
+    plt.plot(range(1,len(valid_loss)+1),valid_loss,label='Validation Loss')
+
+    # validation loss의 최저값 지점을 찾기
+    minposs = valid_loss.index(min(valid_loss))+1
+    plt.axvline(minposs, linestyle='--', color='r',label='Early Stopping Checkpoint')
+
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.ylim(0, 0.5) # 일정한 scale
+    plt.xlim(0, len(train_loss)+1) # 일정한 scale
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    fig.savefig('loss_plot.png', bbox_inches = 'tight')
