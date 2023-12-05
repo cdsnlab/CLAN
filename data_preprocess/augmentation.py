433da51e0d01cab49ac4d449dfa967afdeb7974a
iplay93 <iplay93@gmail.com>
2023-12-05 12:54:02 +0900

    add 'Quantize'

--- data_preprocess/augmentation.py
+++ data_preprocess/augmentation.py
@@ -45,4 +45,6 @@ def select_transformation(aug_method, seq_len):
         my_aug = (Dropout(p=0.1,fill=0))       
     elif(aug_method == 'Pool'):
         my_aug = Pool(kind='max',size=4)
+    elif(aug_method == 'Quantize'):
+        my_aug = Quantize(n_levels=20)
 
