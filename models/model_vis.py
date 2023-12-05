f7b0b9c5e79364cb1ef449b35e9c31b1071d846b
iplay93 <iplay93@gmail.com>
2023-12-05 12:31:27 +0900

    add 'transformer_encoder_f'

--- models/model_vis.py
+++ models/model_vis.py
@@ -22,3 +22,4 @@ class ConTF(nn.Module):
         self.shift_cls_layer_t = nn.Linear(configs.TSlength_aligned * configs.input_channels, args.K_shift)
 
         encoder_layers_f = TransformerEncoderLayer(configs.TSlength_aligned_2, dim_feedforward=2*configs.TSlength_aligned_2,nhead=1, batch_first = True)
+        self.transformer_encoder_f = TransformerEncoder(encoder_layers_f, 2)
