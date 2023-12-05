dc7e23a06bd50d3417258a55fc9820cc4fa4fe42
iplay93 <iplay93@gmail.com>
2023-12-05 12:18:31 +0900

    add transformer arch

--- models/model.py
+++ models/model.py
@@ -60,3 +60,7 @@ class ConTF(nn.Module):
 class ClsTF(nn.Module):
     def __init__(self, configs, args):
         super(ClsTF, self).__init__()
+        self.training_mode = args.training_mode
+
+        encoder_layers_t = TransformerEncoderLayer(configs.TSlength_aligned, dim_feedforward=2*configs.TSlength_aligned, nhead=1, batch_first = True)
+        self.transformer_encoder_t = TransformerEncoder(encoder_layers_t, 2)
