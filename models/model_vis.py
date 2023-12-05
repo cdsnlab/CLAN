fe61634343530b897a805e292eb088411de53cc5
iplay93 <iplay93@gmail.com>
2023-12-05 12:27:56 +0900

    edit 'transformer_encoder_t'

--- models/model_vis.py
+++ models/model_vis.py
@@ -10,3 +10,4 @@ class ConTF(nn.Module):
     def __init__(self, configs, args):
         super(ConTF, self).__init__()
         encoder_layers_t = TransformerEncoderLayer(configs.TSlength_aligned, dim_feedforward=2*configs.TSlength_aligned, nhead=1, batch_first = True)
+        self.transformer_encoder_t = TransformerEncoder(encoder_layers_t, 2)
