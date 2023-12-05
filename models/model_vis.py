90181c8d7fa06a4f848fb5322b49f278cd475c76
iplay93 <iplay93@gmail.com>
2023-12-05 12:26:43 +0900

    edit class ConTF

--- models/model_vis.py
+++ models/model_vis.py
@@ -4,3 +4,6 @@ from .Transformer import TransformerEncoder, TransformerEncoderLayer
 import torch.nn.functional as F
 import seaborn
 import matplotlib.pyplot as plt
+
+# Encoders for novelty detection
+class ConTF(nn.Module):
