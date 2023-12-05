716546cff37a2f43398be6921f157d96bf9fb2b2
iplay93 <iplay93@gmail.com>
2023-12-05 12:46:44 +0900

    edit select_transformation

--- data_preprocess/augmentation.py
+++ data_preprocess/augmentation.py
@@ -2,7 +2,7 @@ from tsaug import *
 
 def select_transformation(aug_method, seq_len):
     if(aug_method == 'AddNoise'):
-        my_aug = (AddNoise(scale=0.01))
+        my_aug = AddNoise(scale=0.01)
     elif(aug_method == 'Convolve'):
         my_aug = Convolve(window="flattop", size=11)
     elif(aug_method == 'Crop'):
