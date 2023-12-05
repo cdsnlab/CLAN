fc9497295942f4f70c89f6ed79de800b5e4d60ab
iplay93 <iplay93@gmail.com>
2023-12-05 12:11:50 +0900

    add descriptions

--- models/model.py
+++ models/model.py
@@ -2,6 +2,7 @@ from torch import nn
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 import torch.nn.functional as F
 
+# encoders for novelty detection
 class ConTF(nn.Module):
     def __init__(self, configs, args):
         super(ConTF, self).__init__()
