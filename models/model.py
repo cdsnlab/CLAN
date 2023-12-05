93004bd68057861606bc879546a9a5c012a455b3
iplay93 <iplay93@gmail.com>
2023-12-05 12:19:43 +0900

    add classification model's output layers

--- models/model.py
+++ models/model.py
@@ -17,7 +17,7 @@ class ConTF(nn.Module):
         )
         self.shift_cls_layer_t = nn.Linear(configs.TSlength_aligned * configs.input_channels, args.K_shift)
 
-        encoder_layers_f = TransformerEncoderLayer(configs.TSlength_aligned_2, dim_feedforward=2*configs.TSlength_aligned_2,nhead=1)
+        encoder_layers_f = TransformerEncoderLayer(configs.TSlength_aligned_2, dim_feedforward=2*configs.TSlength_aligned_2,nhead=1, batch_first = True)
         self.transformer_encoder_f = TransformerEncoder(encoder_layers_f, 2)
 
         self.projector_f = nn.Sequential(
@@ -62,5 +62,13 @@ class ClsTF(nn.Module):
         super(ClsTF, self).__init__()
         self.training_mode = args.training_mode
 
-        encoder_layers_t = TransformerEncoderLayer(configs.TSlength_aligned, dim_feedforward=2*configs.TSlength_aligned, nhead=1, batch_first = True)
-        self.transformer_encoder_t = TransformerEncoder(encoder_layers_t, 2)
+        encoder_layers = TransformerEncoderLayer(configs.TSlength_aligned, dim_feedforward=2*configs.TSlength_aligned, nhead=1, batch_first = True)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, 2)
+
+        self.projector = nn.Sequential(
+            nn.Linear(configs.TSlength_aligned * configs.input_channels, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 128)
+        )
+        self.shift_cls_layer = nn.Linear(configs.TSlength_aligned * configs.input_channels, 1)
