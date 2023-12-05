c65af4f2cc70f205691c6f867ffb53edb5f6cda9
iplay93 <iplay93@gmail.com>
2023-12-05 11:28:10 +0900

    change model class

--- model/model.py
+++ model/model.py
@@ -1,2 +1,7 @@
+from torch import nn
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 import torch.nn.functional as F
+
+class ConTF(nn.Module):
+    def __init__(self, configs, args):
+        super(ConTF, self).__init__()
