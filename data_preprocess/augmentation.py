4bf6345f29a0f5cacd82ac52e97b876d7be94cc7
iplay93 <iplay93@gmail.com>
2023-12-05 12:46:30 +0900

    add Crop

--- data_preprocess/augmentation.py
+++ data_preprocess/augmentation.py
@@ -5,3 +5,7 @@ def select_transformation(aug_method, seq_len):
         my_aug = (AddNoise(scale=0.01))
     elif(aug_method == 'Convolve'):
         my_aug = Convolve(window="flattop", size=11)
+    elif(aug_method == 'Crop'):
+        my_aug = PERMUTE(min_segments=1, max_segments=5, seg_mode="random")
+   
+
