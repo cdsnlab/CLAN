4f313fc9d127aa4c863b0971f7c653b3652d3277
iplay93 <iplay93@gmail.com>
2023-12-05 12:27:33 +0900

    add 'encoder_layer'

--- models/model_vis.py
+++ models/model_vis.py
@@ -9,3 +9,4 @@ import matplotlib.pyplot as plt
 class ConTF(nn.Module):
     def __init__(self, configs, args):
         super(ConTF, self).__init__()
+        encoder_layers_t = TransformerEncoderLayer(configs.TSlength_aligned, dim_feedforward=2*configs.TSlength_aligned, nhead=1, batch_first = True)
