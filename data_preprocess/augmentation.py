7c0a5afcaf7c0b46784c3f6b35c2193bdd3b525e
iplay93 <iplay93@gmail.com>
2023-12-05 12:46:03 +0900

    add 'Convolve'

--- data_preprocess/augmentation.py
+++ data_preprocess/augmentation.py
@@ -3,3 +3,5 @@ from tsaug import *
 def select_transformation(aug_method, seq_len):
     if(aug_method == 'AddNoise'):
         my_aug = (AddNoise(scale=0.01))
+    elif(aug_method == 'Convolve'):
+        my_aug = Convolve(window="flattop", size=11)
