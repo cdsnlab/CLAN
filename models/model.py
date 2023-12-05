1a5cf7862b2b2f6762d1b76a72358eb9c7a88d7e
iplay93 <iplay93@gmail.com>
2023-12-05 12:06:10 +0900

    edit output layers

--- models/model.py
+++ models/model.py
@@ -7,3 +7,11 @@ class ConTF(nn.Module):
         super(ConTF, self).__init__()
         encoder_layers_t = TransformerEncoderLayer(configs.TSlength_aligned, dim_feedforward=2*configs.TSlength_aligned, nhead=1, batch_first = True)
         self.transformer_encoder_t = TransformerEncoder(encoder_layers_t, 2)
+
+        self.projector_t = nn.Sequential(
+            nn.Linear(configs.TSlength_aligned * configs.input_channels, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 128)
+        )
+        self.shift_cls_layer_t = nn.Linear(configs.TSlength_aligned * configs.input_channels, args.K_shift)
