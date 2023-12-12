0bd0b4da8967321bee4cb5155da78bf89cf2e717
iplay93 <iplay93@gmail.com>
2023-12-12 12:48:28 +0900

    label list append

--- data_preprocess/aras_preprocess.py
+++ data_preprocess/aras_preprocess.py
@@ -87,4 +87,6 @@ def arasLoader(file_name, timespan, min_seq):
                 if(current_label[0] != temp_df[i, 20]):          
                     dataset_list.append(TSDataSet(temp_dataset, (current_label[0]), len(temp_dataset)))                
                 else:
-                    dataset_list.append(TSDataSet(temp_dataset, (current_label[1]), len(temp_dataset)))              
+                    dataset_list.append(TSDataSet(temp_dataset, (current_label[1]), len(temp_dataset)))  
+
+                label_list.append(current_label)            
