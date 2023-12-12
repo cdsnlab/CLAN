14effc1c7970145ba383fa368da3974e05d60c19
iplay93 <iplay93@gmail.com>
2023-12-12 12:32:20 +0900

    add TSdataset

--- data_preprocess/aras_preprocess.py
+++ data_preprocess/aras_preprocess.py
@@ -2,3 +2,8 @@ from glob import glob
 import numpy as np
 import pandas as pd
 
+class TSDataSet:
+    def __init__(self,data, label, length):
+        self.data = data
+        self.label = int(label)
+        self.length= int(length)
