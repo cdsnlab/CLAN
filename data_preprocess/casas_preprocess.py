20bc032f0dec972057f968bd4fc26494a90e4892
iplay93 <iplay93@gmail.com>
2023-12-12 12:51:55 +0900

    add casasLoader

--- data_preprocess/casas_preprocess.py
+++ data_preprocess/casas_preprocess.py
@@ -12,3 +12,4 @@ class TSDataSet:
 # file name = day 
 # Examples(txt) : 2008-11-10 14:28:17.986759 M22 ON 2 2 
 # [1, 4, 6, 9, 10, 12, 14, 13, 2, 3, 7, 8, 11, 15, 5] : activity #, [50, 26, 27, 24, 26, 34, 26, 43, 26, 26, 26, 24, 27, 26, 17] : # of activities
+def casasLoader(file_name,timespan, min_seq):
