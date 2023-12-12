316d577637cd9feb9875e911ebce87a3ee118473
iplay93 <iplay93@gmail.com>
2023-12-12 12:51:46 +0900

    add casas info

--- data_preprocess/casas_preprocess.py
+++ data_preprocess/casas_preprocess.py
@@ -7,3 +7,8 @@ class TSDataSet:
         self.data = data
         self.label = int(label)
         self.length= int(length)
+
+# CASAS data format : timestamp(when activated), sensor type+context name, state, user #, activity label : [1-15] 
+# file name = day 
+# Examples(txt) : 2008-11-10 14:28:17.986759 M22 ON 2 2 
+# [1, 4, 6, 9, 10, 12, 14, 13, 2, 3, 7, 8, 11, 15, 5] : activity #, [50, 26, 27, 24, 26, 34, 26, 43, 26, 26, 26, 24, 27, 26, 17] : # of activities
